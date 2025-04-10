8b3b22ee34199b02e4a7f5fea25480107a2930e9
Jithran <info@jithransikken.nl>
2025-04-10 21:56:52 +0200

    -fix: optimize the system bounty redraw

--- EWB-Tracker/Views/DefaultView.xaml.cs
+++ EWB-Tracker/Views/DefaultView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using SharedLibrary.Models;
@@ -26,13 +27,43 @@ public partial class DefaultView : UserControl
     {
         Dispatcher.Invoke(() =>
         {
-            var eveSystems = _fileWatcherService.GetEveSystemsList();
+            var newSystems = _fileWatcherService.GetEveSystemsList();
 
-            EveSystemCollection.Clear();
-            foreach (var system in eveSystems)
+            foreach (var newSystem in newSystems)
             {
-                EveSystemCollection.Add(system);
+                var existingSystem = EveSystemCollection.FirstOrDefault(
+                    s => s.EveSystemDto.Name == newSystem.EveSystemDto.Name
+                );
+
+                if (existingSystem != null)
+                {
+                    // Update existing system
+                    existingSystem.Bounty = newSystem.Bounty;
+                    existingSystem.LastUpdated = newSystem.LastUpdated;
+                }
+                else
+                {
+                    // Add the system, it's not in the list
+                    EveSystemCollection.Add(newSystem);
+                }
+            }
+
+            // Sort by lastUpdated field
+            var sorted = EveSystemCollection
+                .OrderByDescending(s => s.LastUpdated)
+                .ToList();
+
+            // Reorder the collection
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var item = sorted[i];
+                var currentIndex = EveSystemCollection.IndexOf(item);
+                if (currentIndex != i)
+                {
+                    EveSystemCollection.Move(currentIndex, i);
+                }
             }
         });
     }
+
 }

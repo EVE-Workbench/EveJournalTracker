8d4e6ff2453f5fca4b66dc55f9f29d16e1ac3c4d
Jithran <info@jithransikken.nl>
2025-04-11 16:17:47 +0200

    feat(model): EveSystem implements INotifyPropertyChanged for ui updates

--- SharedLibrary/Models/EveSystem.cs
+++ SharedLibrary/Models/EveSystem.cs
@@ -1,12 +1,45 @@
-﻿using SharedLibrary.Models.Database;
+﻿using System.ComponentModel;
+using SharedLibrary.Models.Database;
 
 namespace SharedLibrary.Models;
 
-public class EveSystem
+public class EveSystem : INotifyPropertyChanged
 {
+    private int _bounty;
+    private DateTime _lastUpdated;
+
     public EveSystemDto EveSystemDto { get; set; }
-    
-    public int Bounty { get; set; }
-    
-    public DateTime LastUpdated { get; set; }
+
+    public int Bounty
+    {
+        get => _bounty;
+        set
+        {
+            if (_bounty != value)
+            {
+                _bounty = value;
+                OnPropertyChanged(nameof(Bounty));
+            }
+        }
+    }
+
+    public DateTime LastUpdated
+    {
+        get => _lastUpdated;
+        set
+        {
+            if (_lastUpdated != value)
+            {
+                _lastUpdated = value;
+                OnPropertyChanged(nameof(LastUpdated));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }

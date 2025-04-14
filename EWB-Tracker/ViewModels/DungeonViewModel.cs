047ffc4387ee8bc1c2fa6287cce60466406cae19
Jithran <info@jithransikken.nl>
2025-04-14 23:15:27 +0200

    wip(styles): component styles

--- EWB-Tracker/ViewModels/DungeonViewModel.cs
+++ EWB-Tracker/ViewModels/DungeonViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using SharedLibrary.Enums;
 
 namespace EWB_Tracker.ViewModels;
 
-public class DungeonViewModel
+public class DungeonViewModel : INotifyPropertyChanged
 {
     public ObservableCollection<string> DungeonTypes { get; set; }
-    public string SelectedDungeonType { get; set; }
+    public ObservableCollection<string> DungeonNames { get; set; } = new() { "Outpost", "Stronghold", "Refinery" };
+    public ObservableCollection<string> Factions { get; set; } = new() { "Sansha", "Blood Raiders", "Guristas" };
+    public ObservableCollection<string> Levels { get; set; } = new() { "1", "2", "3", "4", "5" };
+
+    private string _selectedDungeonType;
+    public string SelectedDungeonType
+    {
+        get => _selectedDungeonType;
+        set
+        {
+            _selectedDungeonType = value;
+            OnPropertyChanged(nameof(SelectedDungeonType));
+            OnPropertyChanged(nameof(IsCustomSelected));
+            OnPropertyChanged(nameof(IsStandardSelected));
+        }
+    }
+
+    public bool IsCustomSelected => SelectedDungeonType == "Custom";
+    public bool IsStandardSelected => !string.IsNullOrEmpty(SelectedDungeonType) && SelectedDungeonType != "Custom";
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
     public DungeonViewModel()
     {
@@ -16,5 +40,6 @@ public class DungeonViewModel
         {
             DungeonTypes.Add(dungeonType.ToString());
         }
+        DungeonTypes.Add("Custom");
     }
 }

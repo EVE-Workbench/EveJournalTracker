fa5714ce099cb1f58c218a4464c94aeef39328ef
Jithran <info@jithransikken.nl>
2026-01-09 22:14:19 +0100

    Layout changes -mod: layout changes to settings -mod: window controls layout

--- UI.avalonia/Views/SettingsView.axaml.cs
+++ UI.avalonia/Views/SettingsView.axaml.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using UI.avalonia.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +15,21 @@ public partial class SettingsView : UserControl
         var viewModel = App.ServiceProvider.GetRequiredService<SettingsViewModel>();
         DataContext = viewModel;
         _ = viewModel.InitializeAsync();
-        // TODO: Port full SettingsView logic from WPF project
+    }
+
+    private void OnAccessTokenLinkPressed(object? sender, PointerPressedEventArgs e)
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://evejournal.com/my-account/personal-access-tokens",
+                UseShellExecute = true
+            });
+        }
+        catch
+        {
+            // Silently fail if browser cannot be opened
+        }
     }
 }
